b13cf53be06cb4369c302f0773e1c9a4db45c139
Gabriel Gimenes <gabrielgimenes6@gmail.com>
2018-10-02 22:17:41 -0300

    change css files

---
+++ Projeto/components/aside.php
@@ -0,0 +1,42 @@
+<aside id="laterall">
+<h1>Outras Notícias</h1>
+<h2>Vídeo mais recente</h2>
+
+<video id="video01" controls="controls" poster="_imagens/print-1.png" width="350px" height="200px">
+<source src="_media/the-piano.mp4" type="video/mp4" />
+<source src="_media/the-piano.webm" type="video/webm" />
+<source src="_media/the-piano.ogv" type="video/ogg" />
+desculpe, mas não foi possivel carregar o vídeo.
+</video>
+
+<h2 style="text-align: left;">Novidades</h2>
+<p>
+<font size="3"><h3>Marshmello lança novo clipe confira:</h3></font>
+- Happier
+<video id="video01" controls="controls" poster="_imagens/print2.jpg" width="350px" height="200px">
+<source src="_media/marshmello-happie.mp4" type="video/mp4" />
+<source src="_media/marshmello-happie.webm" type="video/webm" />
+<source src="_media/marshmello-happie.ogv" type="video/ogg" />
+desculpe, mas não foi possivel carregar o vídeo.
+</video>
+98% gostaram desse álbum</br>
+Artista:<a href="https://www.google.com.br/search?q=happier+maxence+luchi&stick=H4sIAAAAAAAAAOPgE-LVT9c3NEw3qoyvSi_JUoJwk40Kss0Nygu1xLKTrfRzS4szk_UTc5JKc60Si0oyi0sAKzL1YDgAAAA&sa=X&ved=2ahUKEwj6-5vSttzdAhUBQhoKHabrBpcQmxMoATAaegQIChAW&biw=1600&bih=794" target="_blank"> Maxence Luchi</br></a>
+Lançamento: 17 de agosto de 2018</br>
+Gravadora:<a href="https://www.google.com.br/search?q=happier+astralwerks&stick=H4sIAAAAAAAAAOPgE-LVT9c3NEw3qoyvSi_JUuLUz9U3MCypKCnWEs1OttLPLS3OTNZPzEkqzbXKSUxKzQEAk-uzRzMAAAA&sa=X&ved=2ahUKEwi-uvjxrdzdAhVNhxoKHSrGBz4QmxMoATAbegQICRAd" target="_blank"> Astralwerks</br></a>
+Gênero: Dance/Eletrônica</BR></br>
+
+<font size="3"><h3>Edson & Hudson lança novo clipe confira:</h3></font>
+- Te Amei Pra...
+
+<video id="video01" controls="controls" poster="_imagens/print3.jpg" width="350px" height="200px">
+<source src="_media/Te-Amei-Pra.mp4" type="video/mp4" />
+<source src="_media/Te-Amei-Pra.webm" type="video/webm" />
+<source src="_media/Te-Amei-Pra.ogv" type="video/ogg" />
+desculpe, mas não foi possivel carregar o vídeo.
+</video>
+Artista:<a href="https://www.google.com.br/search?sa=X&q=te+amei+pra...+edson+%26+hudson&stick=H4sIAAAAAAAAAOPgE-LVT9c3NEyzyChPyTUzV-LSz9U3MCw0z6001VLKTrbSzy0tzkzWL0pNzi9KycxLj0_OKS0uSS2ySiwqySwuAQDtmkArQQAAAA&ved=2ahUKEwjVxozZu9zdAhWv4YUKHQwHCWEQmxMoATADegQIBRAO&biw=1600&bih=794" target="_blank"> Edson & Hudson</br></a>
+Data de lançamento: 2018
+Gênero:Sertanejo
+</p>
+
+</aside>
